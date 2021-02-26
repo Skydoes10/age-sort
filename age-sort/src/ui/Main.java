eae4951e477e2718c1c6a2c4a9516475d2130c19
Ricardo Medina <71156336+Skydoes10@users.noreply.github.com>
2021-02-26 18:09:06 -0500

    Method bubbleSort created and implemented

--- age-sort/src/ui/Main.java
+++ age-sort/src/ui/Main.java
@@ -6,7 +6,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
-
 public class Main {
 
 	public static void main(String[] args) throws IOException {
@@ -28,7 +27,7 @@ public class Main {
 			for(int j = 0; j < length; j ++) {
 				ageArray[j] = Double.parseDouble(ageStringArray[j]);
 			}
-			
+			ageArray = bubbleSort(ageArray);
 		}
 		bw.write(output);
 		
@@ -36,5 +35,27 @@ public class Main {
 		bw.close();
 	}
 	
-	
+	public static double[] bubbleSort(double[] ageArray) {
+		double totalChanges = 0;
+		int changes = 1;
+		double[] sortedArray = new double[ageArray.length+1];
+		for(int  i = 1; i < ageArray.length && changes > 0; i++) {
+			changes = 0;
+			for(int j = 0; j < ageArray.length-i; j++) {
+				if(ageArray[j] > ageArray[j+1]) {
+					double temp = ageArray[j];
+					ageArray[j] = ageArray[j+1];
+					ageArray[j+1] = temp;
+					changes++;
+				}
+			}
+			totalChanges += changes;
+		}
+		totalChanges = totalChanges/(ageArray.length-1);
+		sortedArray[0] = totalChanges;
+		for(int i = 0; i < ageArray.length; i++) {
+			sortedArray[i+1] = ageArray[i];
+		}
+		return sortedArray;
+	}
 }
